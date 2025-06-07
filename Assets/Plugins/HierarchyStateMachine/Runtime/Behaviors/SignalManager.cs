1938aa8f1eb1fd69a823d15a74ae3e3bdc50fab1
CBowneUCF <156275809+CBowneUCF@users.noreply.github.com>
2025-06-07 17:19:33 -0400

    Signal Set MidWay

---
+++ Assets/Plugins/HierarchyStateMachine/Runtime/Behaviors/SignalManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using EVENT = UltEvents.UltEvent;
+
+namespace SLS.StateMachineH.V5
+{
+    [RequireComponent(typeof(StateMachine))]
+    public class SignalManager : StateBehavior
+    {
+        public SignalSet globalSignals;
+
+        public EVENT this[string name] => globalSignals[name];
+
+        public bool FireSignal(string name)
+        {
+            if(Machine.CurrentState.TryGetComponent(out SignalNode signalNode) && signalNode.FireSignal(name)) return true;
+            else if (globalSignals.ContainsKey(name))
+            {
+                globalSignals[name]?.InvokeSafe();
+                return true;
+            }
+            return false;
+            
+        }
+
+
+    }
+}
---
+++ Assets/Plugins/HierarchyStateMachine/Runtime/Behaviors/SignalNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using EVENT = UltEvents.UltEvent;
+
+namespace SLS.StateMachineH.V5
+{
+    [RequireComponent(typeof(IStateChild))]
+    public class SignalNode : StateBehavior
+    {
+        public SignalSet signals;
+
+        public EVENT this[string name] => signals[name];
+
+        public bool FireSignal(string signalName)
+        {
+            if (signals.ContainsKey(signalName))
+            {
+                signals[signalName]?.InvokeSafe();
+                return true;
+            }
+            else return false;
+        }
+    }
+
+    public class SignalSet : Dictionary<string, EVENT> { }
+}
